2af99afc9e900cdd812ad41cdecbb2136ccd5cd4
leanlp <0xleansan@gmail.com>
2024-05-06 15:58:42 -0300

    fix constructor params on vestingAddress

--- script/DeployLunarX.s.sol
+++ script/DeployLunarX.s.sol
@@ -10,18 +10,18 @@ import "../src/Dex.sol";
 
 contract RepayScript is Script {
     function run() external {
-        console.log("Repay...");
+        console.log("Deploy...");
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address tokenLunarX = 0x88722fe08849E91eD6dA694B62EFD37885c5dBFd;
-        
-        address tokenUsdt = 0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8;
-        //OWRNERC20//0x72441784AeAFDb42a26880FE4b6e17370029a16c;
-        
-        address vestingAddress = 0x994a67837723B5828096295798b1A154E53b15f1;
-        address owner = 0xd3E7c6032Cb71BAc39C2c16Df41b5364b6158de5;
 
-        address dexAddress =  0x318348C7eef9FbF7942Cea767696f6c05a08af41;
+        address tokenLunarX = 0x88722fe08849E91eD6dA694B62EFD37885c5dBFd;
+        address tokenUsdt = 0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8;
+        address ownerAddress = 0xd3E7c6032Cb71BAc39C2c16Df41b5364b6158de5;
+
+        address vestingAddress = 0xA51f83991C2381C5DdF46033D74FAbaec0DdC8Ce;
+
+        address dexAddress =  0x26835F5168303F5b3Aa60CE5bb55Dfb7105B98c1;
+
         Vesting vestingContract = Vesting(vestingAddress);
         LunarX lunarXContract = LunarX(tokenLunarX);
         UsdT usdTContract = UsdT(tokenUsdt);
@@ -29,24 +29,32 @@ contract RepayScript is Script {
         Dex dexContract = Dex(dexAddress);
 
         vm.startBroadcast(deployerPrivateKey);
-       // LunarX Lunar = new LunarX(0xd3E7c6032Cb71BAc39C2c16Df41b5364b6158de5);
-       // Old
-       //UsdT Usd = new UsdT(0xd3E7c6032Cb71BAc39C2c16Df41b5364b6158de5);
+
+       // LunarX Lunar = new LunarX(ownerAddress);
+       
+       //UsdT Usd = new UsdT(ownerAddress);
                 
-       // Vesting vest = new Vesting(tokenLunarX, 2000, payable(0xd3E7c6032Cb71BAc39C2c16Df41b5364b6158de5));
+       //Vesting vest = new Vesting(tokenLunarX, 2000, payable(ownerAddress));
         
-       // Dex dex = new Dex(payable(0xd3E7c6032Cb71BAc39C2c16Df41b5364b6158de5), vestingAddress, tokenLunarX);
+       // Dex dex = new Dex(payable(ownerAddress), vestingAddress, tokenLunarX);
 
 
-// last call all together
+        /////if owner could calling all together/////
 
-       vestingContract.setDexAddress(dexAddress);
-        lunarXContract.mint(dexAddress, 100e18);
-        usdTContract.approve(dexAddress, 100e6);
+        //vestingContract.setDexAddress(dexAddress);
+
+        //lunarXContract.mint(dexAddress, 1000e18);
+
+        usdTContract.approve(dexAddress, 10e6);
         
-        //usdTContract.mint(owner, 100e6); 
+        dexContract.depositUSDTandReciveToken(10e6);
 
-       dexContract.depositUSDTandReciveToken(1e6);
+
+        /////finish and withdraw
+
+        //dexContract.withdraw(tokenUsdt);
+
+        //vestingContract.withdraw();
 
         vm.stopBroadcast();
     }
--- src/Dex.sol
+++ src/Dex.sol
@@ -2,23 +2,23 @@
 pragma solidity 0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Vesting} from "./Vesting.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// TODO
 // Using SafeTransfer in all transfer
-
+// Pause
 
 contract Dex is ReentrancyGuard, Ownable {
     //address payable public owner;
 
-    IERC20 private  tokenX;
-    IERC20 private  usdt;
-    
-    address vestingAddress= 0x994a67837723B5828096295798b1A154E53b15f1; 
+    IERC20 private tokenX;
+    IERC20 private usdt;
 
-    Vesting private vestingContract = Vesting(vestingAddress);
+    address private vestingAddress;
+
+    Vesting private vestingContract;
 
     uint256 private constant tokenX_PRICE_IN_USDT = 100;
 
@@ -28,33 +28,35 @@ contract Dex is ReentrancyGuard, Ownable {
     // keeps track of individuals' USDC balances
     mapping(address => uint256) public usdtBalances;
 
+    /// @dev tokens ERC20
+    // @param tokenX token to delivery, ERC20 with 18 decimals
+    // @param usdT ERC20 with 6 decimals
 
-
-/// @dev tokens ERC20
-// @param tokenX token to delivery, ERC20 with 18 decimals
-// @param usdT ERC20 with 6 decimals
-
-    constructor(address payable initialOwner, address _vestingAddress, address _tokenX) Ownable(initialOwner) {
+    constructor(
+        address payable initialOwner,
+        address _vestingAddress,
+        address _tokenX
+    ) Ownable(initialOwner) {
         //owner = (initialOwner);   //payable(msg.sender);
         tokenX = IERC20(_tokenX);
-        usdt = IERC20(0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8);    
+        usdt = IERC20(0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8);
         vestingAddress = _vestingAddress;
+        vestingContract = Vesting(vestingAddress);
     }
 
-    function depositUSDTandReciveToken(uint256 _amount) nonReentrant public {
+    function depositUSDTandReciveToken(uint256 _amount) public nonReentrant {
         usdtBalances[msg.sender] += _amount;
 
         uint256 allowance = usdt.allowance(msg.sender, address(this));
         require(allowance >= _amount, "Check the token allowance");
         usdt.transferFrom(msg.sender, address(this), _amount);
-        
+
         uint256 tokenToReceive = ((usdtBalances[msg.sender] *
             tokenX_PRICE_IN_USDT) * 1e12);
-            usdtBalances[msg.sender] -= _amount;
+        usdtBalances[msg.sender] -= _amount;
         tokenX.transfer(address(vestingContract), tokenToReceive);
         address beneficiary = msg.sender;
         vestingContract.lock(tokenToReceive, beneficiary);
-
     }
 
     function getBalance(address _tokenAddress) public view returns (uint256) {
@@ -66,4 +68,3 @@ contract Dex is ReentrancyGuard, Ownable {
         token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 }
-
