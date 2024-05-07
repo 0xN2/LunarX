3015e977c17e1730a9e834414ca82a9088556dc7
leanlp <0xleansan@gmail.com>
2024-05-07 09:47:15 -0300

    safeTransfer

--- src/Dex.sol
+++ src/Dex.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // TODO
-// Using SafeTransfer in all transfer
 // Pause
 
 contract Dex is ReentrancyGuard, Ownable {
@@ -49,12 +48,15 @@ contract Dex is ReentrancyGuard, Ownable {
 
         uint256 allowance = usdt.allowance(msg.sender, address(this));
         require(allowance >= _amount, "Check the token allowance");
-        usdt.transferFrom(msg.sender, address(this), _amount);
+        //usdt.transferFrom(msg.sender, address(this), _amount);
+        SafeERC20.safeTransferFrom(usdt, msg.sender, address(this), _amount);
 
         uint256 tokenToReceive = ((usdtBalances[msg.sender] *
             tokenX_PRICE_IN_USDT) * 1e12);
         usdtBalances[msg.sender] -= _amount;
-        tokenX.transfer(address(vestingContract), tokenToReceive);
+        //tokenX.transfer(address(vestingContract), tokenToReceive);
+        SafeERC20.safeTransfer(tokenX, address(vestingContract), tokenToReceive);
+
         address beneficiary = msg.sender;
         vestingContract.lock(tokenToReceive, beneficiary);
     }
@@ -65,6 +67,7 @@ contract Dex is ReentrancyGuard, Ownable {
 
     function withdraw(address _tokenAddress) public onlyOwner {
         IERC20 token = IERC20(_tokenAddress);
-        token.transfer(msg.sender, token.balanceOf(address(this)));
+        //token.transfer(msg.sender, token.balanceOf(address(this)));
+        SafeERC20.safeTransfer(token, msg.sender, token.balanceOf(address(this)));
     }
 }
