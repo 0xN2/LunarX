c6a5d203e20374cc1a2b2ffa59ef706ec2375f81
“0xn2” <0xleansan@gmail.com>
2024-05-20 11:23:53 -0300

    Vesting test

--- test/Vesting.t.sol
+++ test/Vesting.t.sol
@@ -2,72 +2,102 @@
 pragma solidity 0.8.24;
 
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../src/Vesting.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
- import "forge-std/Vm.sol";
-
-contract MockToken is ERC20 {
-    constructor() ERC20("MockToken", "MTK") {
-        _mint(msg.sender, 1000000 * 10 ** 18);
-    }
-}
+import "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract VestingTest is Test {
     Vesting vesting;
-    MockToken token;
-    address beneficiary;
-      
+    ERC20Mock token;
+    address owner = address(0x1);
+    address beneficiary = address(0x2);
+    address dexAddress = address(0x3);
+    uint256 expiryDuration = 1 weeks;
 
     function setUp() public {
-        token = new MockToken();
-        beneficiary = address(0x1);
-        vesting = new Vesting(address(token), 365 days, payable(address(this)));
+        token = new ERC20Mock();
+        vm.prank(owner);
+        vesting = new Vesting(address(token), expiryDuration, owner);
 
-        // Grant the Vesting contract some tokens to work with
-        token.transfer(address(vesting), 500000 * 10 ** 18);
+        // Mint tokens to owner and approve to vesting contract
+        token.mint(owner, 10 ether);
+
+        // Transfer some tokens to the contract for testing
+        vm.prank(owner);
+        token.transfer(address(vesting), 10 ether);
     }
 
-    function testLock() public {
-        uint256 amountToLock = 1000 * 10 ** 18;
-
-        // Act as the owner of the contract
-        vm.startPrank(address(this));
-
-        vesting.lock(amountToLock, beneficiary);
-        
-        // Check the locked amount is correct
-        uint256 lockedAmount = vesting.getBeneficiaryAmount(beneficiary);
-        assertEq(lockedAmount, amountToLock, "Locked amount is incorrect.");
-
+    function testLockTokens() public {
+        vm.startPrank(owner);
+        vesting.lock(10 ether, beneficiary);
         vm.stopPrank();
+
+        assertEq(vesting.getBeneficiaryAmount(beneficiary), 10 ether);
     }
 
-    function testWithdraw() public {
-        uint256 amountToLock = 1000 * 10 ** 18;
-
-        // Act as the owner and lock some tokens
-        vm.startPrank(address(this));
-        vesting.lock(amountToLock, beneficiary);
+    function testWithdrawTokens() public {
+        // Lock tokens first
+        vm.startPrank(owner);
+        vesting.lock(10 ether, beneficiary);
         vm.stopPrank();
 
-        // Fast forward time to after the expiry
-        vm.warp(block.timestamp + 366 days);
+        // Move time forward to after the expiry
+        vm.warp(block.timestamp + expiryDuration + 1);
 
-        // Act as the beneficiary
+        // Beneficiary withdraws tokens
         vm.startPrank(beneficiary);
         vesting.withdraw();
+        vm.stopPrank();
 
-        // Check the beneficiary received the tokens
-        uint256 balance = token.balanceOf(beneficiary);
-        assertEq(balance, amountToLock, "Withdrawal amount is incorrect.");
+        assertEq(token.balanceOf(beneficiary), 10 ether);
+    }
+
+    function testEmergencyWithdrawal() public {
+        vm.startPrank(owner);
+        uint256 initialOwnerBalance = token.balanceOf(owner);
+        vesting.withdrawEmergencyTokens(10 ether);
+        assertEq(token.balanceOf(owner), initialOwnerBalance + 10 ether);
+    }
+
+    function testPauseUnpause() public {
+        vm.startPrank(owner);
+        vesting.pause();
+        vm.expectRevert(0xd93c0665);
+        vesting.lock(10 ether, beneficiary);
+        vesting.unpause();
+        vesting.lock(10 ether, beneficiary);
+        assertEq(vesting.getBeneficiaryAmount(beneficiary), 10 ether);
+    }
+
+    function testWithdrawWhenPaused() public {
+        // Lock tokens first
+        vm.startPrank(owner);
+        vesting.lock(10 ether, beneficiary);
+        vm.stopPrank();
+
+        // Move time forward to after the expiry
+        vm.warp(block.timestamp + expiryDuration + 1);
+
+        // Pause the contract
+        vm.startPrank(owner);
+        vesting.pause();
+        vm.stopPrank();
+
+        // Beneficiary tries to withdraw tokens when paused
+        vm.startPrank(beneficiary);
+        vm.expectRevert(0xd93c0665);
+        vesting.withdraw();
+        vm.stopPrank();
+        
+        vm.startPrank((owner));
+        vesting.unpause();
         vm.stopPrank();
     }
-function testFailWithdrawEarly() public {
-        uint256 amount = 500 ether;
-        token.transfer(address(vesting), amount);
-        vesting.lock(amount, beneficiary);
-        vm.warp(block.timestamp + 1 hours);  // Use the existing `vm` instance
-        vesting.withdraw();
+
+    function testSetDexAddress() public {
+        vm.startPrank(owner);
+        vesting.setDexAddress(dexAddress);
+        vm.stopPrank();
+        // assertEq(vesting.dexAddress(), dexAddress);
     }
-   
 }
